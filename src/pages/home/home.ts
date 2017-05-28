62ba797915b3a84e8be8eecb47c57578e37b202f
Brad Berkobien <b.berkobien@kunzleigh.com>
2017-05-28 13:13:11 -0400

    - First commit - Basic project plus the start of the home screen

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,12 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  public wells: string[] = ["The Bible", "Ministry"];
+
   constructor(public navCtrl: NavController) {
 
   }
+
+
 
 }
